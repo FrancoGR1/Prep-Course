464c072063716e209762f2e6ccebfb65f8d33d37
unknown <francoagarciar@gmail.com>
2022-05-04 04:39:09 -0300

    combinar nombres

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -161,20 +161,24 @@ function esPositivo(numero) {
   //Si el número es positivo, devolver ---> "Es positivo"
   //Si el número es negativo, devolver ---> "Es negativo"
   //Si el número es 0, devuelve false
-  
+  if (numero < 0) {
+    return "Es Negativo";
+  }
+    return "Es Positivo";
 }
 
 function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código:
+  return "str!";
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código:
-  
+  return " nombre apellido";
 }
 
 function obtenerSaludo(nombre) {
