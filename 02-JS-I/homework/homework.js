ae91fe3e41822e1f15cb1ef36c20fe2e94552c91
unknown <francoagarciar@gmail.com>
2022-05-03 22:34:48 -0300

    es par

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -67,21 +67,21 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1.length == str2.length);
+  if ( str1.length == str2.length );
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num<90);
+  if ( num < 90 );
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (50<num);
+  if ( 50 < num );
 }
 
 function obtenerResto(x, y) {
